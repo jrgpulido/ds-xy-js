ff42753523f4cda25b6d1b471ed20fc2820d7e95
jrgpulido <jrgpulido@MacBook-Air-de-Mary.local>
2020-10-14 11:56:18 -0500

    First repo commit

---
+++ 1p/0.js
@@ -0,0 +1,26 @@
+// from js, kt, cc, java, py
+// first examples
+//then exercises
+
+// input: Preconditions
+// output: Postconditions
+
+// arguments
+// result
+
+// x*x <- cuadrado(x)
+// 4 <- cuadrado(2)
+
+// isNumber(x)
+//
+// -> x
+// <- true if x is number
+// <- false otherwise
+
+//testing area
+console.log('Hello DS world in ES6+\n')
+
+///
+//from here on
+///
+
