66ae260b0584290796126922167452b814c22539
jrgpulido <jrgpulido@gmail.com>
2021-04-16 10:39:27 -0500

    2p linked first commit

---
+++ 2p/0ds/matrix-onConsole.js
@@ -0,0 +1,50 @@
+//
+//
+//
+let aMatrix = [
+    [1,2],
+    [4,5],
+    [7,8]
+]
+
+console.log(aMatrix[0])   //
+console.log(aMatrix[1][0])//
+console.log(aMatrix[1][4])// ??
+console.log(aMatrix[4])   // ??
+
+//then
+let A = new Array(3)
+A[0]= new Array(3)
+A[0][0]=-2
+//Math.random()
+
+
+//MATRIX
+
+//1. create
+//1.1 fixed
+N=[
+    [3,2,-1],
+    [4,5,3]
+];
+
+
+let M = null;
+let f = 2;
+let c = 3;
+
+//err
+//let A=M+N
+
+//
+M === N
+
+console.log(N[0])
+console.log(M[1])//qm
+N[0]+M[1]
+N[0][0]+M[1][1];//??
+
+//2. declare
+M = new Array(2);
+M[0]=new Array(3);
+M[1]=new Array(3);
---
+++ 2p/4linkedlist/Node4stu.js
@@ -0,0 +1,13 @@
+//
+// 
+//
+
+class Node {
+    constructor(d, n = null){
+	this._data=d
+	this._next=n
+    }
+    get data(){
+	return this._data
+    }
+}
