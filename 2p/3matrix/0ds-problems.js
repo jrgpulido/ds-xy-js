118879c694970522a540af70760d565c26121f57
jrgpulido <jrgpulido@gmail.com>
2021-04-29 12:01:27 -0500

    2p first commit

---
+++ 2p/3matrix/0ds-problems.js
@@ -0,0 +1,52 @@
+/** INSTRUCTIONS
+ * 
+ * create one file by program
+ * implement a method, function, non-function solution
+ * solve each as requested
+ * may use code from previous exercises
+ * 
+ * PROBLEM 1
+ * insertAt(p,v)
+ *
+ * inserts a value v, at position p, in the array
+ * @arg {number} p - position
+ * @arg {number} v - value
+ *
+ * @example
+ * insertAt(3,3)
+ * Verify operation result by outputing the array before and after
+ *
+ * PROBLEM 2
+ * removeAt(p)
+ *
+ * removes value at position p
+ * @arg {number} p - position
+ *
+ * @example
+ * removeAt(3)
+ *
+ * PROBLEM 3
+ * insertFirst(v)
+ *
+ * inserts value v at first position
+ * @arg {number} v - value
+ *
+ * @example
+ * insertFirst(4)
+ * may wrap method unshift
+ * Verify operation result by outputing the array before and after
+ *
+ * PROBLEM 4
+ * removeLast()
+ *
+ * removes value at last position
+ * @arg {number} v - value
+ *
+ * @example
+ * removeLast()
+ * may wrap method pop
+ * Verify operation result by outputing the array before and after
+ *
+ * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
+ * @see {@link https://jsdoc.app}
+ */
---
+++ 2p/3matrix/forOf4stu.js
@@ -0,0 +1,5 @@
+let anArray = [2, 3, 4, 5, 6]
+
+for (let x of anArray) {
+  console.log(x)
+}
