e9fda327559f574474b2f9418c7ae9f76007a14b
jrgpulido <jrgpulido@gmail.com>
2021-04-30 22:01:49 -0500

    2p 3matrix commit

--- 2p/3matrix/arraysMethods4stu.js
+++ 2p/3matrix/arraysMethods4stu.js
@@ -1,10 +1,11 @@
 //
 // what it does : one by one
 //
+
 let numbers = [ 3, 2, 1]
 console.log(numbers)
 
-//
+// 
 numbers.push(10, 11)
 console.log(numbers)
 
@@ -12,15 +13,15 @@ console.log(numbers)
 numbers.pop()
 console.log(numbers)
 
-//
+//vs pop
 numbers.shift()
 console.log(numbers)
 
-//
+//vs push
 numbers.unshift( -2, -1, 0)
 console.log(numbers)
 
-//remove/separate
+//remove/separate/splice
 let ns = numbers.splice(2,3)
 console.log(numbers)
 console.log(ns)
