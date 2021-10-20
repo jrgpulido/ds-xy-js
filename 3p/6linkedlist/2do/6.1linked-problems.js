1a0daae33080f45a3370acc32ba165eb5bdd6632
jrgpulido <jrgpulido@gmail.com>
2021-10-20 18:22:13 -0500

    linked exercise commit

---
+++ 3p/6linkedlist/2do/6.1linked-problems.js
@@ -0,0 +1,16 @@
+/** INSTRUCTIONS
+ * 
+ * create one file by program
+ * implement a method, function, non-function solution
+ * solve each as requested
+ * may use code from previous exercises
+ * 
+ * PROBLEM 1
+ * size()
+ *
+ * returns the size of the linked-list
+ * @returns {number} - size
+ *
+ * @example
+ * size()
+ * Verify operation result
