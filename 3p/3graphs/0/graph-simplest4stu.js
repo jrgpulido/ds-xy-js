dfd17cbdbefe200817becdbdbae7e39eee0b7473
JRG Pulido on MDP <jrgpulido@gmail.com>
2021-11-23 12:38:31 -0600

    graph update 1 commit

---
+++ 3p/3graphs/0/graph-simplest4stu.js
@@ -0,0 +1,14 @@
+//
+//
+//
+
+let friends = {
+    "Alice" : ["Bob", "Diana", "Fred"],
+    "Bob" : ["Alice", "Cynthia", "Diana"],
+    "Cynthia" : ["Bob"],
+    "Diana" : ["Alice", "Bob", "Fred"],
+    "Elise" : ["Fred"],
+    "Fred" : ["Alice", "Diana", "Elise"]
+}
+
+console.log(friends['Fred'])
