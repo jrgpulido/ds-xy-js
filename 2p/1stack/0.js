8614d44cc73b436db3a86226bfdac1f1c274d86e
JRG Pulido on DCompLab <jrgpulido@gmail.com>
2022-03-11 17:15:00 -0600

    array-based stack example

--- 2p/1stack/0.js
+++ 2p/1stack/0.js
@@ -1,3 +1,27 @@
 //your code here
 
 console.log('Hello stack DS world in ES6+\n')
+
+let a=[]
+
+a.push('a')
+console.log(a)
+
+a.push('b')
+console.log(a)
+
+a.push('d')
+console.log(a)
+
+a.forEach(k=>console.log(k))
+
+a.pop()
+console.log(a)
+
+a.pop()
+console.log(a)
+console.log(a.length)
+
+a.pop()
+console.log(a)
+console.log(a.length)
