92917ac61bd4962f77691adbcab5b4849fafe402
JRG Pulido on MDP <jrgpulido@gmail.com>
2022-03-15 19:53:54 -0600

    exercise directory organizing

---
+++ 2p/1sorting/2do/2.1sorting-problems.js
@@ -0,0 +1,29 @@
+//
+// create one file by program
+// implement a method, function, non-function solution
+// solve each as requested
+// may use code from previous exercises
+//
+// SORTING
+
+
+//1. mergesort
+//   sort(d)
+// -> d an array of random numbers
+// <- the random numbers sorted
+
+//2. quicksort
+//   sort(d)
+// -> d an array of random numbers
+// <- the random numbers sorted
+
+//3. Array.sort()
+
+// forEach
+// + verify is sorted
+// + print
+// - time it took
+// - steps it tooks
+
+// create a 5-column table
+
