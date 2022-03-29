b1a379074310d688cbc32f09ac15fa11bfa25306
JRG Pulido on DCompLab <jrgpulido@gmail.com>
2022-03-28 18:22:00 -0600

    queue code+exercises

--- 2p/4queue/2do/4.0problems.js
+++ 2p/4queue/2do/4.0problems.js
@@ -1,3 +1,12 @@
-//
-// pending
-//
+/** INSTRUCTIONS
+ *
+ * create one file by program
+ * implement a method, function, non-function solution
+ * solve each as requested
+ * may use code from previous exercises
+ *
+ * PROBLEM 1
+ * half-problem
+ * a array-based queue implementation
+ *
+ */
