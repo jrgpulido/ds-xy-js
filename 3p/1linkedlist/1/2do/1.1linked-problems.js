12c58ef5055f5b0a288e1ee7078c16a7b570e385
JRG Pulido on MDP <jrgpulido@gmail.com>
2022-10-13 19:43:04 -0500

    linked++ exercise and directory updating

--- 3p/1linkedlist/1/2do/1.1linked-problems.js
+++ 3p/1linkedlist/1/2do/1.1linked-problems.js
@@ -9,12 +9,22 @@
  * contains(v)
  *
  * look up for value v in the linked list
- * @arg {type} v - value; may be of a char, number, u object
- * @returns {bool} - true or false, if value v exist or not
+ * @arg {type} v - value; may be of a char, number, or object
+ * @returns {bool} - true or false, if value v exists or not,
  * may use traverse
  *
  * @example
  * contains(3)
+ *
+ * PROBLEM 2
+ * getTail()
+ *
+ * retrieve the node at the tail
+ * @returns {Node} - Node at the tail or null if the list is empty
+ *
+ * @example
+ * getTail()
+ *
  *
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
