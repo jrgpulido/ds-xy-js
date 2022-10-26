e188c3753eb9078e0bec63a67afce399caf7c5a3
Hector Antonio <TheSoityn_Mx@outlook.com>
2022-10-25 21:53:01 -0500

    Update 1.8.1 (Actualizacion: 8impoirt is RUN)

--- 2p/5linked/8imports/useNode5es.js
+++ 2p/5linked/8imports/useNode5es.js
@@ -7,3 +7,46 @@ let list = new Node()
 list.prepend('b')
 
 console.log(list)
+
+//
+// console.log('\n******************************')
+//
+// list.append('a')
+// list.append([1, 2, 3, 4, 5])
+// console.log(list)
+//
+// console.log('\n******************************')
+//
+// list.prepend('MyHead')
+// console.log(list)
+//
+// console.log('\n******************************')
+//
+// console.log(list.getHead())
+//
+// console.log('\n******************************')
+//
+// console.log(list.getTail())
+//
+// console.log('\n******************************')
+//
+// console.log(list.toString())
+//
+// console.log('\n******************************')
+//
+// list.traverse()
+//
+// console.log('\n******************************')
+//
+// list.contains('a')
+//
+// console.log('\n******************************')
+//
+// list.insertAfter('c', 'b')
+// console.log(list)
+//
+// console.log('\n******************************')
+// console.log("*-**-**-**-**-**-**-**-**-**-*")
+//
+// list.insertBefore('c', 'b')
+// list.traverse()
