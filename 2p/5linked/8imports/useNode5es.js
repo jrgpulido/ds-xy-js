6eee9f7ba6962011ee36026d22ee33d4d9a68f46
Hector Antonio <TheSoityn_Mx@outlook.com>
2022-10-25 21:38:47 -0500

    Update 1.8.0 (Actualizacion: The LinkedList is Full)

--- 2p/5linked/8imports/useNode5es.js
+++ 2p/5linked/8imports/useNode5es.js
@@ -10,30 +10,44 @@ let list = new LinkedList()
 list.append('a')
 console.log(list)
 
-console.log("******************************")
+console.log('\n******************************')
 
 list.append('b')
 list.append([1, 2, 3, 4, 5])
 console.log(list)
 
-console.log("******************************")
+console.log('\n******************************')
 
-list.prepend([123,'abc'])
+list.prepend('MyHead')
 console.log(list)
 
-console.log("******************************")
+console.log('\n******************************')
 
 console.log(list.getHead())
 
-console.log("******************************")
+console.log('\n******************************')
 
 console.log(list.getTail())
 
-console.log("******************************")
+console.log('\n******************************')
 
 console.log(list.toString())
 
-console.log("******************************")
+console.log('\n******************************')
+
+list.traverse()
+
+console.log('\n******************************')
+
+list.contains('b')
+
+console.log('\n******************************')
+
+list.insertAfter('c', 'b')
+console.log(list)
+
+console.log('\n******************************')
 console.log("*-**-**-**-**-**-**-**-**-**-*")
 
+list.insertBefore('c', 'b')
 list.traverse()
--- haas/linked/node5es.js
+++ haas/linked/node5es.js
@@ -13,17 +13,17 @@ function Node(data) { // Constructor de nodos
 }
 
 /////////////////////////////////////////////////////
-// Tested - OK
+// Tested - OK - getHead
 LinkedList.prototype.getHead = function () { // Devuelve el primer nodo de la lista
     return this.head.data
 }
 
-// Tested - OK
+// Tested - OK - getTail
 LinkedList.prototype.getTail = function () { // Devuelve el último nodo de la lista
     return this.tail
 }
 
-// Tested - OK
+// Tested - OK - prepend
 LinkedList.prototype.prepend = function (data) { // Añade un nodo al principio de la lista
     if (!this.head) {
         this.head = new Node(data)
@@ -36,7 +36,7 @@ LinkedList.prototype.prepend = function (data) { // Añade un nodo al principio 
     }
 }
 
-// Tested - OK
+// Tested - OK - append
 LinkedList.prototype.append = function (data) { // Añade un nodo al final de la lista
     if (!this.head) {
         this.head = new Node(data)
@@ -52,7 +52,7 @@ LinkedList.prototype.append = function (data) { // Añade un nodo al final de la
     }
 }
 
-// Tested - OK
+// Tested - OK - toString
 LinkedList.prototype.toString = function () { // Devuelve la lista en formato string
     let referencia = this.head;
     let cadena = ""
@@ -63,7 +63,7 @@ LinkedList.prototype.toString = function () { // Devuelve la lista en formato st
     return cadena
 }
 
-// Testing - OK
+// Tested - OK - traverse
 LinkedList.prototype.traverse = function () { // Recorre la lista y ejecuta una función callback
     let referencia = this.head;
     let i = 0
@@ -73,6 +73,64 @@ LinkedList.prototype.traverse = function () { // Recorre la lista y ejecuta una 
         i++
     }
     console.log('\n')
+}
+
+// Tested - OK - contains
+LinkedList.prototype.contains = function (data) { // Devuelve true si la lista contiene el valor buscado
+    let referencia = this.head
+    while (referencia) {
+        if (referencia.data === data) {
+            console.log('El nodo [' +data+ '] está en la lista')
+            return true
+        }
+        referencia = referencia.next
+    }
+    console.log('El nodo [' +data+ '] no está en la lista')
+    return false
+}
+
+// Tested - OK - insertAfter
+LinkedList.prototype.insertAfter = function (input, data) { // Añade un nodo después de otro
+    let referencia = this.head
+    let newNode = new Node(input)
+    while (referencia) {
+        if (referencia.data === data) {
+            newNode.next = referencia.next
+            referencia.next = newNode
+
+            console.log('El nodo [' +input+ '] se ha insertado después del nodo [' +data+ ']' )
+            return true
+        }
+        referencia = referencia.next
+    }
+    console.log('El nodo [' +data+ '] no está en la lista')
+    return false
+}
+
+// Tested - OK - insertBefore - ERROR en Arrays
+LinkedList.prototype.insertBefore = function (input, data) { // Añade un nodo antes de otro
+
+    let referencia = this.head
+    let newNode = new Node(input)
+
+    if (referencia.data === data) {
+        newNode.next = referencia
+        this.head = newNode
+
+        console.log('El nodo [' +input+ '] se ha insertado antes del nodo [' +data+ ']' )
+        return true
+    }
+    while (referencia) {
+
+        if (referencia.next.data === data) {
+            newNode.next = referencia.next
+            referencia.next = newNode
+
+            console.log('El nodo [' + input + '] se ha insertado antes del nodo [' + data + ']')
+            return true
+        }
+        referencia = referencia.next
+    }
 }
 
 // exporta la lista enlazada
