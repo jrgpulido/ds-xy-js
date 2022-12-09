b93548b6c5c5a29a323e0233768cd8a5532baee5
SANCHEZ REYES REYES <112107149+ReneSanchez03@users.noreply.github.com>
2022-12-09 22:42:21 +0000

    El ejercicio 500coin.js ahora es funcional.

--- O/500coin.js
+++ O/500coin.js
@@ -1,9 +1,9 @@
 function coin() {
   n = Math.random()
   if (n < 0.5)
-    return 'cara'
+    return 'cara'//cara = 1
   else
-    return 'sol'
+    return 'sol'//sol = 0
 }
 
 console.log(coin())
@@ -13,6 +13,6 @@ console.log(coin())
 console.log(coin())
 
   for (let i = 0; i < 500; i++) {
-  const num = Math.floor(Math.random() * 0.5);
+  const num = Math.floor(Math.random() < 0.5);
   console.log(num);
 }
