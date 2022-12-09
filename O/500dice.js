d2cd94298083d37e0364e6c9657deef1147916b3
SANCHEZ REYES REYES <112107149+ReneSanchez03@users.noreply.github.com>
2022-12-09 06:55:15 +0000

    Ejercicio del dado 500 veces realizado.

---
+++ O/500dice.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < 500; i++) {
+  const num = Math.floor(Math.random() * 6.0) + 1;
+  console.log(num);
+}
--- O/dice.js
+++ O/dice.js
@@ -2,15 +2,15 @@ let n = 1
 let reclama = 2
 
 let caraAbajo = 5/6
-let singleOdds = 1/6
+let posibilidades = 1/6
 console.log('Numero del dado:', n)
 let posibles = Math.pow(6, n)
 console.log('Posibles salidas:', posibles)
  
 for (let i=0; i < n; i++) {
   if (i == 0) continue
-  singleOdds = singleOdds * singleOdds
+  posibilidades = posibilidades * posibilidades
 }
-console.log(singleOdds * reclama)
+console.log(posibilidades * reclama)
  
-console.log(singleOdds)
+console.log(posibilidades)
