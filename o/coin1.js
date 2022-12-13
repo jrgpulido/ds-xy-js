1579f6a775a0fd07cc0d66470d15b9ec64a21157
JRG Pulido <jrgpulido@gmail.com>
2022-12-13 23:41:51 +0000

    some more exercises

---
+++ o/coin1.js
@@ -0,0 +1,20 @@
+//
+//
+//
+
+function coin() {
+    n=Math.random()
+
+    if (n<0.5){
+        return 'cara'
+    }
+    else{
+        return 'sol'
+    }
+}
+
+console.log(coin())
+console.log(coin())
+console.log(coin())
+console.log(coin())
+console.log(coin())
