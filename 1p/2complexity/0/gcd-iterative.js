4440ff89cba43e5ac216d599e236d9bcda34b23b
jrgpulido <jrgpulido@gmail.com>
2023-08-28 19:45:39 +0000

    minor file updating

--- 1p/2complexity/0/gcd-iterative.js
+++ 1p/2complexity/0/gcd-iterative.js
@@ -1,3 +1,29 @@
 //
-// https://ideone.com/00FoC9
+// gcd - greatest common divisor
+// also known as Euclid's algorithm
 //
+const gcd=(a, b) => {
+    let u = a
+    let v = b
+    let r = 0
+    while (u > 0) {
+        if (u < v) {
+            r = u
+            u = v
+            v = r
+        }
+        u -= v
+    }
+    console.log('print here how many steps it took')
+    return v
+}
+
+console.time('it took')
+console.log('gcd(1, 1e3) =',gcd(1, 1000000))//1
+console.log('print here how much time it took')
+console.timeEnd('it took')
+
+//then try
+//console.log(gcd(3, 2))
+//console.log(gcd(1000000005, 999999999))
+//can we do better ? (yes/no)
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 //
-// **
+//
 //
 console.log('Hello ES6+ DS world')
